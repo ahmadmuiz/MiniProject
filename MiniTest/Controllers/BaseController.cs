a78e714b7b027550ac26d9bbf121dcd1922f643e
ahmadmuiz <ahmalidi@gmail.com>
2014-04-04 22:30:28 +0700

    Mini Project
    Working Project

---
+++ MiniTest/Controllers/BaseController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiniTest.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected bool SessionAdministrator
+        {
+            get
+            {
+                bool result = false;
+                if (Session["SessionAdministrator"] == null) return false;
+                bool.TryParse(Session["SessionAdministrator"].ToString(), out result);
+                return result;
+            }
+            set
+            {
+                Session["SessionAdministrator"] = value;
+            }
+        }
+
+        protected bool IsAdministrator
+        {
+            get
+            {
+                return false;
+            }
+            set
+            {
+
+            }
+        }
+
+    }
+}
---
+++ MiniTest/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiniTest.Controllers
+{
+    public class HomeController: BaseController
+    {
+        public ActionResult Index()
+        {
+            if (!IsAdministrator)
+            {
+                return RedirectToAction("Login");
+            }else 
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return PartialView();
+        }
+    }
+}
